f0758c33471e10de9522d0d0c021181209441cf3
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-05 15:03:17 -0600

    test: get recipes for searched country

---
+++ app/services/recipe_service.rb
@@ -0,0 +1,18 @@
+class RecipeService
+  def search(query)
+    get_url("api/recipes/v2?q=#{query}")
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://api.edamam.com/') do |f|
+      f.params['app_id'] = ENV['EDAMAM_APP_ID']
+      f.params['app_key'] = ENV['EDAMAM_APP_KEY']
+      f.params['type'] = "public"
+    end
+  end
+end
---
+++ spec/facades/recipe_facade_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe RecipeFacade do
+  describe "class methods" do
+    describe "::recipes_by_country" do
+      context "happy path" do
+        it "makes recipes to be serialized", :vcr do
+          query = "Thailand"
+          recipes = RecipeFacade.new.recipes_by_country(query)
+
+          expect(recipes).to be_an(Array)
+
+          recipes.each do |recipe|
+            expect(recipe).to be_a(Recipe)
+            expect(recipe.country).to eq(query)
+            expect(recipe.image).to be_a(String)
+            expect(recipe.title).to be_a(String)
+            expect(recipe.url).to be_a(String)
+          end
+        end
+      end
+    end
+  end
+end
