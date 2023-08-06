3009ed8f1cbd481f395ba0476088624c7ca9f9c1
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-05 22:16:04 -0600

    feat: list results for empty search and sad path

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,8 +2,14 @@ module Api
   module V1
     class RecipesController < ApplicationController
       def index
-        recipes = RecipeFacade.new.recipes_by_country(params[:country])
-        render json: RecipeSerializer.new(recipes)
+        if params[:country] == ""
+          random_country = CountryFacade.new.random_country
+          recipes = RecipeFacade.new.recipes_by_country(random_country.name)
+          render json: RecipeSerializer.new(recipes), status: :ok
+        else
+          recipes = RecipeFacade.new.recipes_by_country(params[:country])
+          render json: RecipeSerializer.new(recipes), status: :ok
+        end
       end
     end
   end
