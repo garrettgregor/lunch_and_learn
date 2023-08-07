7bea8261dd3fd6aa24a7f71a51f40bf570338803
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-07 11:49:06 -0600

    extension: add random country for empty query

--- app/controllers/api/v1/air_quality_controller.rb
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -2,9 +2,14 @@ module Api
   module V1
     class AirQualityController < ApplicationController
       def index
-        capital = CountryFacade.new.capital_of(params[:country])
-        air_quality = AirQualityFacade.new.air_quality_in(capital.name)
-
+        if params[:country] == ""
+          random_country = CountryFacade.new.random_country
+          capital = CountryFacade.new.capital_of(random_country.name)
+          air_quality = AirQualityFacade.new.air_quality_in(capital.name)
+        else
+          capital = CountryFacade.new.capital_of(params[:country])
+          air_quality = AirQualityFacade.new.air_quality_in(capital.name)
+        end
         render json: AirQualitySerializer.serialize(capital, air_quality), status: :ok
       end
     end
