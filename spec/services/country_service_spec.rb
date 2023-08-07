fa1ec7770d5e172d357065d1449c501cb9c246ba
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-07 10:05:26 -0600

    test: add capital city to service

--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -14,5 +14,35 @@ RSpec.describe CountryService do
         end
       end
     end
+
+    describe "::capital_of" do
+      context "happy path" do
+        it "returns a Nigeria's capital as a list", :vcr do
+          query = "Nigeria"
+          result = CountryService.new.capital_of(query)
+
+          expect(result).to be_an(Array)
+
+          capital = result.first
+
+          expect(capital).to have_key(:capital)
+          expect(capital[:capital]).to be_an(Array)
+          expect(capital[:capital].first).to eq("Abuja")
+        end
+
+        it "returns a France's capital as a list", :vcr do
+          query = "France"
+          result = CountryService.new.capital_of(query)
+
+          expect(result).to be_an(Array)
+
+          capital = result.first
+
+          expect(capital).to have_key(:capital)
+          expect(capital[:capital]).to be_an(Array)
+          expect(capital[:capital].first).to eq("Paris")
+        end
+      end
+    end
   end
 end
