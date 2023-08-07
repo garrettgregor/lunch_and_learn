6920c6d00aad03bb93dc151aca7722e30f490405
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-07 09:07:18 -0600

    feat: add air quality controller

---
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class AirQualityController < ApplicationController
+      # def index
+      #   if params[:country] == ""
+      #     random_country = CountryFacade.new.random_country
+      #     recipes = RecipeFacade.new.recipes_by_country(random_country.name)
+      #   else
+      #     recipes = RecipeFacade.new.recipes_by_country(params[:country])
+      #   end
+      #   render json: RecipeSerializer.new(recipes), status: :ok
+      # end
+    end
+  end
+end
