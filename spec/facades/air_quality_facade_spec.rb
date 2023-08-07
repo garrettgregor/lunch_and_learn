eb218ba5e16e8c21f5febd8d83b9872e6e352ca9
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-07 10:07:41 -0600

    test: air quality facade

---
+++ spec/facades/air_quality_facade_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe AirQualityFacade do
+  describe "class methods" do
+    describe "::air_quality_in" do
+      it "returns an air quality object for a specific capital", :vcr do
+        query = "Abuja"
+        air_quality = AirQualityFacade.new.air_quality_in(query)
+
+        expect(air_quality).to be_an(AirQuality)
+        expect(air_quality.aqi).to be_a(Float)
+        expect(air_quality.pm25_concentration).to be_a(Float)
+        expect(air_quality.co_concentration).to be_a(Float)
+        expect(air_quality.city).to eq(query)
+        expect(air_quality.id).to eq(nil)
+      end
+    end
+  end
+end
