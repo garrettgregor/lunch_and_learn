5f303b87bd6143c57fe69022b3cdbdce1fa7bfe4
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-07 22:09:03 -0600

    leave question about error and sad path handling

--- app/facades/video_facade.rb
+++ app/facades/video_facade.rb
@@ -2,7 +2,13 @@ class VideoFacade
   def video_resources(country)
     results = service.mrhistory_videos(country)
 
-    @video = Video.new(results[:items].first, country)
+    @video = Video.new(results[:items].first)
+    ## Question: how to return an empty hash for an error with no video
+    # if !results[:items].first.nil?
+    #   @video = Video.new(results[:items].first)
+    # else
+    #   return {}
+    # end
   end
 
   private
--- app/serializers/learning_resources_serializer.rb
+++ app/serializers/learning_resources_serializer.rb
@@ -1,8 +1,43 @@
 class LearningResourcesSerializer
-  include JSONAPI::Serializer
-  set_type :learning_resource
+  def self.serialize(country, video, photos)
+    {
+      data:
+      {
+        id: nil,
+        type: "learning_resource",
+        attributes:
+        {
+          country: country,
+          video:
+          ## Question: try to rescue with empty hash for video and empty array for photos
+          # if video == {}
+          #   video: {},
+          # else
+          #   {
+          #     title: video.title,
+          #     youtube_video_id: video.youtube_video_id
+          #   },
+          # end
+          {
+            title: video.title,
+            youtube_video_id: video.youtube_video_id
+          },
+          images: photos.map do |photo|
+            {
+              alt_tag: photo.alt_tag,
+              url: photo.url
+            }
+          end
+        }
+      }
+    }
+  end
 
-  attributes :title, :youtube_video_id, :country, :images
+  ## Question: how to do the same thing with JSONAPI::Serializer
+  # include JSONAPI::Serializer
+  # set_type :learning_resource
+
+  # attributes :title, :youtube_video_id, :country, :images
 
   # attribute :images do |object|
   #   photos = PhotoFacade.new.photos_from
