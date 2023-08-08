5d608709f93f6e2ede92031320a2cadf265a0fca
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-08 15:56:57 -0600

    feat: user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,26 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      def index
+      before_action :new_user
 
+      def create
+        return create_user if @user.save
+        ## Refactor: to account for all possible errors and individual messages
+        render json: ErrorSerializer.new(@user.errors.full_messages).user_errors, status: 404
+      end
+
+      private
+      def user_params
+        params.permit(:name, :email, :password, :password_confirmation)
+      end
+
+      def new_user
+        @user = User.new(user_params)
+      end
+
+      def create_user
+        @user.update(user_params)
+        render json: UserSerializer.new(@user), status: 201
       end
     end
   end
---
+++ app/serializers/error_serializer.rb
@@ -0,0 +1,16 @@
+class ErrorSerializer
+  def initialize(error)
+    @error = error
+  end
+
+  def user_errors
+    {
+      errors: [
+        {
+          status: '404',
+          title: @error
+        }
+      ]
+    }
+  end
+end
