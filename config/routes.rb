21b90af681f828dd99338e851d8b5ade2618bb96
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-08 16:32:29 -0600

    feat: add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
       resources :learning_resources, only: %i[index]
       resources :air_quality, only: %i[index]
       resources :users, only: %i[create]
+      resources :sessions, only: %i[create]
     end
   end
 end
