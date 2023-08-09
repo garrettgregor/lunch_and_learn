755317607c6c4d0026e631cd20a923505205e2f8
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-08 18:47:35 -0600

    feat: add user favorites

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,8 +1,25 @@
 module Api
   module V1
     class FavoritesController < ApplicationController
+      before_action :find_user
+
       def create
-        require 'pry'; binding.pry
+        if @user.present?
+          @user.favorites.create!(favorite_params)
+          render json: { success: "Favorite added successfully" }, status: :created
+        else
+          render json: ErrorSerializer.new("User credentials invalid").credential_errors, status: :unauthorized
+        end
+      end
+
+      private
+
+      def find_user
+        @user = User.find_by(api_key: params[:api_key])
+      end
+
+      def favorite_params
+        params.permit(:country, :recipe_link, :recipe_title)
       end
     end
   end
