255d14cfe7e35ab21ab16bdd9941c1b097880a9f
Garrett Gregor <118634754+garrettgregor@users.noreply.github.com>
2025-03-24 19:38:20 -0600

    upgrade to rails 8 and fix vulns (#10)
    * utilize dotenv gem
    
    * update ruby version
    
    * update bundler
    
    * chores: upgrade to rails 8

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
+Rails.application.config.active_support.to_time_preserves_timezone = :zone
+
 # Initialize the Rails application.
 Rails.application.initialize!
