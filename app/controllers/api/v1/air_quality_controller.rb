d14a38a77a56adb1559587cb9b77bc6dea4cd00a
Garrett Gregor <118634754+garrettgregor@users.noreply.github.com>
2025-03-24 20:14:10 -0600

    chores: add rubocops and fix violations (#11)
    * style: fix rubocop -a violations
    
    * chores: add rubocops
    
    * update ci

--- app/controllers/api/v1/air_quality_controller.rb
+++ app/controllers/api/v1/air_quality_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class AirQualityController < ApplicationController
       def index
-        country = (params[:country].presence || CountryFacade.new.random_country.name)
+        country = params[:country].presence || CountryFacade.new.random_country.name
         capital = CountryFacade.new.capital_of(country)
         air_quality = AirQualityFacade.new.air_quality_in(capital.name)
         render json: AirQualitySerializer.serialize(capital, air_quality), status: :ok
